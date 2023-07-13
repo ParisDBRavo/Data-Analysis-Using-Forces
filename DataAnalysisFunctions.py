4798c56b8db497b7bc1ee05329f8b2b6e2533862
El Paris <Paadab112@gmail.com>
2023-07-13 00:42:30 -0600

    --amend

--- DataAnalysisFunctions.py
+++ DataAnalysisFunctions.py
@@ -3,8 +3,19 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import itertools
 def is_categorical(array_like):
     return array_like.dtype.name == 'category'
+
+def savecsv(dataset,name,artifact_name, flag=False):
+    cwd = Path.cwd()
+    out_path_files = Path(cwd, "Data/Out/"+artifact_name)
+    file_save = out_path_files / name
+    if flag:
+        dataset.to_csv(file_save, float_format="%.4f", index=False)
+    else:
+        dataset.to_csv(file_save, encoding = 'utf-8-sig')
 
 def df_to_plotly(df):
     return {'z': df.values.tolist(),
@@ -31,9 +42,6 @@ def get_first_specific_type(dataset,artifact_name):
     for i, col_name in enumerate(dataset.columns):
         if len(col_name) > 2 and col_name!="Latitude"and col_name!="Longitude":
             first_long_col = i
-            if artifact_name=="ring":
-                print(first_long_col)
-                print(col_name)
             break
         first_long_col= int(dataset.shape[1]/2)-1
     if first_long_col==1:
@@ -60,3 +68,114 @@ def save_images_heatmaps(dataset_G_cs,dataset_S_cs, out_path_im):
     file_save = out_path_im / ("CS_S.png")
     plt.savefig(file_save, dpi=900)
     plt.close()
+
+def get_clean_dataset(dataset_I, artifact_name):
+    one_site=[]
+    one_type = []
+    dataset_I_IM = dataset_I[dataset_I['UltimateForm']==artifact_name]
+    #remane sites to lower case
+    dataset_I_IM["SiteName"] = dataset_I_IM["SiteName"].str.lower()
+    if len(set(dataset_I_IM["SiteName"])) ==1:
+        one_site.append(artifact_name)
+    dataset_I_IM['ArtifactTypeNameGeneral'] = [word[:2].upper() for word in dataset_I_IM.ArtifactTypeName]
+    if len(set(dataset_I_IM["ArtifactTypeNameGeneral"])) ==1:
+        one_type.append(artifact_name)
+    ArtifactTypeName = pd.get_dummies(dataset_I_IM.ArtifactTypeName)
+    ArtifactTypeNameGeneral = pd.get_dummies(dataset_I_IM.ArtifactTypeNameGeneral)
+    dataset_locs = pd.concat([dataset_I_IM.SiteName, dataset_I_IM.Latitude, dataset_I_IM.Longitude], axis=1).groupby(['SiteName']).mean()
+    dataset_arts = pd.concat([dataset_I_IM.SiteName,ArtifactTypeNameGeneral, ArtifactTypeName], axis=1).groupby(['SiteName']).sum()
+    dataset = pd.concat([dataset_locs,dataset_arts],axis=1)
+    return dataset
+
+def printing_images_general_and_specific(dataset, artifact_name):
+    cwd = Path.cwd()
+    out_path_im = Path(cwd, "Data/OutIm/"+artifact_name)
+    first_specific_type= get_first_specific_type(dataset,artifact_name)
+    plot_scale = 10 
+    for colid in range(2,first_specific_type):
+        fig = go.Figure(data=go.Scatter(
+        x=dataset.Longitude, 
+        y=dataset.Latitude,
+        hovertemplate =
+        '<b>%{text}</b>'+
+        '<br>Longitude: %{x:.2f}'+
+        '<br>Latitude: %{y:.2f}<br>', 
+        text=dataset.index.tolist(),
+        mode =   'markers',
+        marker = dict(
+            size = dataset.iloc[:,colid].to_numpy()*plot_scale,
+            color = dataset.iloc[:,colid].to_numpy()*plot_scale, #set color equal to a variable
+            #color = labels, #set color equal to a variable
+            colorscale='Inferno', # one of plotly colorscales
+            showscale=True
+        )
+        )) 
+        fig.update_layout(yaxis_range=[dataset.Latitude.min()-1,dataset.Latitude.max()+1])
+        fig.update_layout(xaxis_range=[dataset.Longitude.min()-1,dataset.Longitude.max()+1])
+        file_save = out_path_im / (artifact_name+"_artgeneral_" + dataset.iloc[:,colid].name + ".html")
+        fig.write_html(file_save)
+    plot_scale = 10
+    for colid in range(first_specific_type,dataset.shape[1] ):
+        fig = go.Figure(data=go.Scatter(
+            x=dataset.Longitude, 
+            y=dataset.Latitude,
+            hovertemplate =
+            '<b>%{text}</b>'+
+            '<br>Longitude: %{x:.2f}'+
+            '<br>Latitude: %{y:.2f}<br>', 
+            text=dataset.index.tolist(),
+            mode =   'markers',
+            marker = dict(
+                size = dataset.iloc[:,colid].to_numpy()*plot_scale,
+                color = dataset.iloc[:,colid].to_numpy()*plot_scale, #set color equal to a variable
+                #color = labels, #set color equal to a variable
+                colorscale='Inferno', # one of plotly colorscales
+                showscale=True
+            )
+        )) 
+        fig.update_layout(yaxis_range=[dataset.Latitude.min()-1,dataset.Latitude.max()+1])
+        fig.update_layout(xaxis_range=[dataset.Longitude.min()-1,dataset.Longitude.max()+1])
+        file_save = out_path_im / (artifact_name+"_art_" + dataset.iloc[:,colid].name + ".html")
+        fig.write_html(file_save)
+
+def percentage_of_types_located_in_sites(dataset, artifact_name):
+    first_specific_type= get_first_specific_type(dataset,artifact_name)
+    d = {'col1': [np.zeros(1)]}
+    dataset_art = pd.DataFrame(data=d, index=dataset.iloc[:,2:first_specific_type].columns.tolist())
+    rowid = 0
+    for colid in range(2, first_specific_type):
+        dataset_art.iloc[rowid,0] = ((dataset.iloc[:,colid] != 0).sum()/len(dataset))*100
+        rowid = rowid + 1
+    savecsv(dataset_art, ('dataset_'+artifact_name+'_GeneralType.csv'),artifact_name)
+
+    d = {'col1': [np.zeros(1)]}
+    dataset_art = pd.DataFrame(data=d, index=dataset.iloc[:,first_specific_type:dataset.shape[1]].columns.tolist())
+    rowid = 0
+    for colid in range(first_specific_type, dataset.shape[1]):
+        dataset_art.iloc[rowid,0] = ((dataset.iloc[:,colid] != 0).sum()/len(dataset))*100
+        rowid = rowid + 1
+    savecsv(dataset_art,('dataset_'+artifact_name+'_SpecificType.csv'), artifact_name)
+
+def get_dataset_g_and_s_sharing_objects_between_sites(dataset_Gbin,dataset_Sbin,dataset,dataset_bin):
+    dataset_G_cs = pd.DataFrame(columns=dataset.index.tolist(), index=dataset.index.tolist(), dtype=np.float32)
+    dataset_S_cs = pd.DataFrame(columns=dataset.index.tolist(), index=dataset.index.tolist(), dtype=np.float32)
+    N=dataset_Gbin.shape[1]
+    M=dataset_Sbin.shape[1]
+    for pair in itertools.combinations(dataset_bin.index.tolist(), 2):
+        dataset_G_cs.loc[pair[0], pair[1]]= np.count_nonzero((dataset_Gbin.loc[pair[0],:])&(dataset_Gbin.loc[pair[1],:]))/N
+        dataset_G_cs.loc[pair[1], pair[0]] = dataset_G_cs.loc[pair[0], pair[1]]
+        dataset_S_cs.loc[pair[0], pair[1]]= np.count_nonzero((dataset_Sbin.loc[pair[0],:])&(dataset_Sbin.loc[pair[1],:]))/M
+        dataset_S_cs.loc[pair[1], pair[0]] = dataset_S_cs.loc[pair[0], pair[1]]
+    
+    return dataset_G_cs,dataset_S_cs
+def save_general_edges(artifact_name,dataset_G_csb):
+    dataset_G_csb = dataset_G_csb.reset_index(drop=True)
+    dataset_G_csb.columns = range(dataset_G_csb.columns.size)
+    dataset_G_csb = dataset_G_csb.mask(np.triu(np.ones_like(dataset_G_csb, dtype=bool),0))
+    dataset_G_csb = dataset_G_csb.unstack()
+    dataset_G_csb = dataset_G_csb[~dataset_G_csb.isin([0, np.nan, np.inf, -np.inf])]
+    dataset_G_csb = dataset_G_csb.to_frame()
+    dataset_G_csb = dataset_G_csb.reset_index()
+    dataset_G_csb.rename(columns={'level_0': 'Source', 'level_1': 'Target', 0: 'weight'}, inplace=True)
+    savecsv(dataset_G_csb,(artifact_name+'_edgesGB.csv'), artifact_name)
+    return dataset_G_csb
--- Main.py
+++ Main.py
@@ -1,10 +1,3 @@
-import os
-import seaborn as sns
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import plotly.express as px
-import plotly.io as pio
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
 import pandas as pd
@@ -23,8 +16,6 @@ from sklearn.metrics.pairwise import cosine_similarity
 import itertools
 import statsmodels.formula.api as smf
 
-#from datamodeller import cramers_v, _inf_nan_str, cramers_vc
-
 # create a new context for the decimal format
 ctx = decimal.Context()
 ctx.prec = 4
@@ -40,99 +31,19 @@ out_path_gen.mkdir(parents=True, exist_ok=True)
 dataset_I=DataAnalysisFunctions.prepare_dataset(file_open)
 all_names= set(dataset_I['UltimateForm'])
 DataAnalysisFunctions.create_directories(all_names, cwd)
-one_site=[]
-one_type = []
 for artifact_name in all_names:
-    out_path_files = Path(cwd, "Data/Out/"+artifact_name)
     out_path_im = Path(cwd, "Data/OutIm/"+artifact_name)
-    dataset_I_IM = dataset_I[dataset_I['UltimateForm']==artifact_name]
-    #remane sites to lower case
-    dataset_I_IM["SiteName"] = dataset_I_IM["SiteName"].str.lower()
-    if len(set(dataset_I_IM["SiteName"])) ==1:
-        one_site.append(artifact_name)
-    dataset_I_IM['ArtifactTypeNameGeneral'] = [word[:2].upper() for word in dataset_I_IM.ArtifactTypeName]
-    if len(set(dataset_I_IM["ArtifactTypeNameGeneral"])) ==1:
-        one_type.append(artifact_name)
-    ArtifactTypeName = pd.get_dummies(dataset_I_IM.ArtifactTypeName)
-    ArtifactTypeNameGeneral = pd.get_dummies(dataset_I_IM.ArtifactTypeNameGeneral)
-    dataset_locs = pd.concat([dataset_I_IM.SiteName, dataset_I_IM.Latitude, dataset_I_IM.Longitude], axis=1).groupby(['SiteName']).mean()
-    dataset_arts = pd.concat([dataset_I_IM.SiteName,ArtifactTypeNameGeneral, ArtifactTypeName], axis=1).groupby(['SiteName']).sum()
-    dataset = pd.concat([dataset_locs,dataset_arts],axis=1)
-    name='dataset_'+artifact_name+'.csv'
-    file_save = out_path_files / name
-    dataset.to_csv(file_save, encoding = 'utf-8-sig')
-    #scatter plots
-    plot_scale = 10 
+    dataset = DataAnalysisFunctions.get_clean_dataset(dataset_I,artifact_name)
+    DataAnalysisFunctions.savecsv(dataset,'dataset_'+artifact_name+'.csv',artifact_name)
+    #scatter plots    
     first_specific_type= DataAnalysisFunctions.get_first_specific_type(dataset,artifact_name)
-    for colid in range(2,first_specific_type):
-        fig = go.Figure(data=go.Scatter(
-        x=dataset.Longitude, 
-        y=dataset.Latitude,
-        hovertemplate =
-        '<b>%{text}</b>'+
-        '<br>Longitude: %{x:.2f}'+
-        '<br>Latitude: %{y:.2f}<br>', 
-        text=dataset.index.tolist(),
-        mode =   'markers',
-        marker = dict(
-            size = dataset.iloc[:,colid].to_numpy()*plot_scale,
-            color = dataset.iloc[:,colid].to_numpy()*plot_scale, #set color equal to a variable
-            #color = labels, #set color equal to a variable
-            colorscale='Inferno', # one of plotly colorscales
-            showscale=True
-        )
-        )) 
-        fig.update_layout(yaxis_range=[dataset.Latitude.min()-1,dataset.Latitude.max()+1])
-        fig.update_layout(xaxis_range=[dataset.Longitude.min()-1,dataset.Longitude.max()+1])
-        file_save = out_path_im / (artifact_name+"_artgeneral_" + dataset.iloc[:,colid].name + ".html")
-        fig.write_html(file_save)
-    plot_scale = 10
-    for colid in range(first_specific_type,dataset.shape[1] ):
-        #print(dataset_bells.iloc[:,colid].name)
-        fig = go.Figure(data=go.Scatter(
-            x=dataset.Longitude, 
-            y=dataset.Latitude,
-            hovertemplate =
-            '<b>%{text}</b>'+
-            '<br>Longitude: %{x:.2f}'+
-            '<br>Latitude: %{y:.2f}<br>', 
-            text=dataset.index.tolist(),
-            mode =   'markers',
-            marker = dict(
-                size = dataset.iloc[:,colid].to_numpy()*plot_scale,
-                color = dataset.iloc[:,colid].to_numpy()*plot_scale, #set color equal to a variable
-                #color = labels, #set color equal to a variable
-                colorscale='Inferno', # one of plotly colorscales
-                showscale=True
-            )
-        )) 
-        fig.update_layout(yaxis_range=[dataset.Latitude.min()-1,dataset.Latitude.max()+1])
-        fig.update_layout(xaxis_range=[dataset.Longitude.min()-1,dataset.Longitude.max()+1])
-        file_save = out_path_im / (artifact_name+"_art_" + dataset.iloc[:,colid].name + ".html")
-        fig.write_html(file_save)
-    d = {'col1': [np.zeros(1)]}
-    dataset_art = pd.DataFrame(data=d, index=dataset.iloc[:,2:first_specific_type].columns.tolist())
-    rowid = 0
-    for colid in range(2, first_specific_type):
-        dataset_art.iloc[rowid,0] = ((dataset.iloc[:,colid] != 0).sum()/len(dataset))*100
-        rowid = rowid + 1
-    file_save = out_path_files / ('dataset_'+artifact_name+'_GeneralType.csv')
-    dataset_art.to_csv(file_save, encoding = 'utf-8-sig')
+    DataAnalysisFunctions.printing_images_general_and_specific(dataset, artifact_name)
+    #Sending info to a csv
+    DataAnalysisFunctions.percentage_of_types_located_in_sites(dataset, artifact_name)
 
-    d = {'col1': [np.zeros(1)]}
-    dataset_art = pd.DataFrame(data=d, index=dataset.iloc[:,first_specific_type:dataset.shape[1]].columns.tolist())
-    rowid = 0
-    for colid in range(first_specific_type, dataset.shape[1]):
-        dataset_art.iloc[rowid,0] = ((dataset.iloc[:,colid] != 0).sum()/len(dataset))*100
-        rowid = rowid + 1
-    file_save = out_path_files / ('dataset_'+artifact_name+'_SpecificType.csv')
-    dataset_art.to_csv(file_save, encoding = 'utf-8-sig') 
-
-    
     dataset_L = dataset.iloc[:,0:2]
     dataset_G = dataset.iloc[:,2:first_specific_type]
     dataset_S = dataset.iloc[:,first_specific_type:dataset.shape[1]]
-
 
     dataset_bin = dataset.copy()
     a =dataset_bin.iloc[:,2:dataset.shape[1]]
@@ -142,28 +53,9 @@ for artifact_name in all_names:
     dataset_Gbin = dataset_bin.iloc[:,2:first_specific_type]
     dataset_Sbin = dataset_bin.iloc[:,first_specific_type:dataset.shape[1]]
 
-
-    dataset_G_cs = pd.DataFrame(columns=dataset.index.tolist(), index=dataset.index.tolist(), dtype=np.float32)
-    dataset_S_cs = pd.DataFrame(columns=dataset.index.tolist(), index=dataset.index.tolist(), dtype=np.float32)
-    N=dataset_Gbin.shape[1]
-    M=dataset_Sbin.shape[1]
-    if artifact_name=="ring":
-        dataset_Gbin.to_csv(out_path_gen/"Gbin.csv",encoding = 'utf-8-sig')
-        dataset_Sbin.to_csv(out_path_gen/"Sbin.csv",encoding = 'utf-8-sig')
-    if artifact_name == "bell":
-        dataset_Sbin.to_csv(out_path_gen/"bell_Sbin.csv",encoding = 'utf-8-sig')
-
-    for pair in itertools.combinations(dataset_bin.index.tolist(), 2):
-        dataset_G_cs.loc[pair[0], pair[1]]= np.count_nonzero((dataset_Gbin.loc[pair[0],:])&(dataset_Gbin.loc[pair[1],:]))/N
-        dataset_G_cs.loc[pair[1], pair[0]] = dataset_G_cs.loc[pair[0], pair[1]]
-        dataset_S_cs.loc[pair[0], pair[1]]= np.count_nonzero((dataset_Sbin.loc[pair[0],:])&(dataset_Sbin.loc[pair[1],:]))/M
-        dataset_S_cs.loc[pair[1], pair[0]] = dataset_S_cs.loc[pair[0], pair[1]]
-    if artifact_name=="ring":
-        dataset_G_cs.to_csv(out_path_gen/"G_csbin.csv",encoding = 'utf-8-sig')
-        dataset_S_cs.to_csv(out_path_gen/"S_csbin.csv",encoding = 'utf-8-sig')
     if flag:
         DataAnalysisFunctions.save_images_heatmaps(dataset_G_cs,dataset_S_cs, out_path_im)
-
+    dataset_G_cs,dataset_S_cs=DataAnalysisFunctions.get_dataset_g_and_s_sharing_objects_between_sites(dataset_Gbin,dataset_Sbin,dataset,dataset_bin)
     dataset_G_csb = dataset_G_cs.copy()
     dataset_S_csb = dataset_S_cs.copy()
     dataset_G_csb[dataset_G_csb>0]=1
@@ -172,28 +64,12 @@ for artifact_name in all_names:
     dataset_G_csb[dataset_G_csb==2] = 10
     conections_totals = dataset_G_cs+dataset_S_cs
     conections_totals = conections_totals/conections_totals.max().max()
-    
     nodes = pd.DataFrame(data=np.count_nonzero(dataset_Sbin, axis =1), index=dataset_Sbin.index)
     nodes = nodes.reset_index().reset_index()
     nodes.columns = ['ID','label', 'weight']
-    file_save = out_path_files / (artifact_name+'_nodesG.csv')
-    nodes.to_csv(file_save, float_format="%.4f", index=False, encoding='utf-8-sig') 
-
-    if artifact_name=="ring":
-        print(nodes)
-
-    dataset_G_csb = dataset_G_csb.reset_index(drop=True)
-    dataset_G_csb.columns = range(dataset_G_csb.columns.size)
-    dataset_G_csb = dataset_G_csb.mask(np.triu(np.ones_like(dataset_G_csb, dtype=bool),0))
-    dataset_G_csb = dataset_G_csb.unstack()
-    dataset_G_csb = dataset_G_csb[~dataset_G_csb.isin([0, np.nan, np.inf, -np.inf])]
-    dataset_G_csb = dataset_G_csb.to_frame()
-    dataset_G_csb = dataset_G_csb.reset_index()
-    dataset_G_csb.rename(columns={'level_0': 'Source', 'level_1': 'Target', 0: 'weight'}, inplace=True)
-    file_save = out_path_files / (artifact_name+'_edgesGB.csv')
-    dataset_G_csb.to_csv(file_save, float_format="%.4f", index=False) 
-    if artifact_name=="ring":
-        print(dataset_G_csb)
+    DataAnalysisFunctions.savecsv(nodes,(artifact_name+'_nodesG.csv'), artifact_name)
+    
+    dataset_G_csb=DataAnalysisFunctions.save_and_calculate_general_edges(artifact_name,dataset_G_csb)
 
     conections_totals = conections_totals.reset_index(drop=True)
     conections_totals.columns = range(conections_totals.columns.size)
@@ -203,13 +79,12 @@ for artifact_name in all_names:
     conections_totals = conections_totals.to_frame()
     conections_totals = conections_totals.reset_index()
     conections_totals.rename(columns={'level_0': 'Source', 'level_1': 'Target', 0: 'weight'}, inplace=True)
-    file_save = out_path_files / (artifact_name+'_edges.csv')
-    conections_totals.to_csv(file_save, float_format="%.4f", index=False) 
-
+    DataAnalysisFunctions.savecsv(conections_totals,(artifact_name+'_edges.csv'),artifact_name,True)
 
     dataset_S_cs = pd.DataFrame(columns=dataset.index.tolist(), index=dataset.index.tolist(), dtype=np.float32)
     dataset_Sbinn=dataset_Sbin
     dataset_Sbinn = dataset_Sbinn.drop(dataset_Sbinn.columns[0], axis = 1)
+    
     if artifact_name=="ring":
         print(dataset_Sbinn)
 
@@ -230,5 +105,4 @@ for artifact_name in all_names:
     conections_totals = conections_totals.to_frame()
     conections_totals = conections_totals.reset_index()
     conections_totals.rename(columns={'level_0': 'Source', 'level_1': 'Target', 0: 'weight'}, inplace=True)
-    file_save = out_path_files / (artifact_name+'_edgesSnA.csv')
-    conections_totals.to_csv(file_save, float_format="%.4f", index=False)
+    DataAnalysisFunctions.savecsv(conections_totals,(artifact_name+'_edgesSnA.csv'),artifact_name,True)
